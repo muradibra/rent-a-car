173d245a589122c529e06ad42813d0ef93eaf4af
muradibra <muradibrahimov495@gmail.com>
2026-05-05 22:39:58 +0400

    fix: update MongoDB connection string and switch bcrypt to bcryptjs
    - Changed the MongoDB connection string to use a new cluster.
    - Replaced bcrypt with bcryptjs for password hashing to improve compatibility.

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -7,7 +7,7 @@ async function main() {
   const username = process.env.DB_USERNAME;
   const password = process.env.DB_PASSWORD;
   await mongoose.connect(
-    `mongodb+srv://${username}:${password}@cluster0.lrrw8.mongodb.net/rent-a-car?retryWrites=true&w=majority&appName=Cluster0`
+    `mongodb+srv://${username}:${password}@cluster0.iz1hytg.mongodb.net/rent-a-car`,
   );
 }
 
--- server/src/utils/bcrypt.ts
+++ server/src/utils/bcrypt.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export function hashPassword(password: string) {
   const salt = bcrypt.genSaltSync(+process.env.BCEYPT_SALT_ROUND!);
